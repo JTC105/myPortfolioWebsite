a5a0a28ff304967506f9401c8552f71d6fb52c53
John Vincent Canseco <jvincecanseco@gmail.com>
2026-09-19 21:50:58 +0800

    Changed vite config root domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/myPortfolioWebsite/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000, // Specify the port for the development server
